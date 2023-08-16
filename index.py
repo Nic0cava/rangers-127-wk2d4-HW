61a0740101d2a7397c2282048557f6a02672277a
Nicolas Cava <nmcava01@gmail.com>
2023-08-16 01:15:32 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,15 @@
+import math
+
+def square_foot(length, width):
+    area = 0
+    area == length * width
+    print(area)
+
+square_foot()
+
+def circumference(r):
+    c = 0
+    c == 2 * math.pi * r
+    print(c)
+
+circumference()
---
+++ module.py
@@ -0,0 +1,11 @@
+import math
+
+def square_foot(length, width):
+    area = length * width
+    print(area)
+
+
+
+def circumference(r):
+    c = 2 * math.pi * r
+    print(c)
